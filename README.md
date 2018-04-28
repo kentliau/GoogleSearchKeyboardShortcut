ce7437d9e0d5c5d189750f5ca9d4d1dfa2034a6d
Kent Liau <kent.jr@me.com>
2018-04-28 19:58:24 +0800

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Keyboard Shortcuts for Google Search
+====================================
+Google removed the keyboard shortcut in search, there are available extension in the extension store that give you this functionality back, but they request extraneous permission or use unnecessary third party libraries. This version of mine use no third party libraries nor request unnecessary permission to work. [Just plain vanilla javascript](http://youmightnotneedjquery.com/).
+
+
+Shortcut Key
+============
+
+* `/`: Focus search box
+* `j`: Select next result or next/previous page
+* `k`: Select previous result or next/previous page
+
+
+Installation
+============
+1. Clone the git repository
+2. In the Chrome extensions window, select "Load unpacked extension" and select the `GoogleShortcuts` directory.
---
+++ script.js
@@ -0,0 +1,49 @@
+// keyCode
+// 13  return
+// 47 BLACKSLASH
+// 104 h
+// 106 j
+// 107 k
+// 108 l
+(function() {
+    var hightlightColor = '#ffc';
+    var idx = 0;
+    var links = document.querySelectorAll('h3.r a, #pnprev, #pnnext');
+    var searchInputBox = document.querySelector('#lst-ib');
+
+    links[idx].focus();
+    links[idx].style.backgroundColor = hightlightColor;
+
+    document.onkeypress = function (e) {
+      e = e || window.event;
+
+      if (e.keyCode == 47) {
+        searchInputBox.focus();
+        e.preventDefault();
+      }
+
+      if (e.keyCode == 106) {
+        links[idx].style.backgroundColor = 'inherit';
+        if (idx >= links.length - 1) {
+          idx = 0;
+        } else {
+          idx++;
+        }
+        links[idx].focus();
+        links[idx].style.backgroundColor = hightlightColor;
+      }
+
+      if (e.keyCode == 107) {
+        links[idx].style.backgroundColor = 'inherit';
+        if (idx == 0) {
+          idx = links.length - 1;
+        } else {
+          idx--;
+        }
+        links[idx].focus();
+        links[idx].style.backgroundColor = hightlightColor;
+      }
+
+    };
+
+})();
